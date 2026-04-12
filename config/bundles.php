ca8bba0dd117a8e1d658d291d533ea8948d6b653
Phillip Hemleb <github@phillip.hemleb.de>
2026-04-12 16:16:24 +0200

    [PIP-27] Create User Entity in Authentication Domain

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
