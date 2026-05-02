2d83a6454eb90e90d61bed833c35e8e0247bd167
Phillip Hemleb <github@phillip.hemleb.de>
2026-05-02 15:10:43 +0200

    Add HealthController

---
+++ src/Infrastructure/Health/Controller/HealthController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Health\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthController
+{
+    #[Route('/health', name: 'health', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
+}
